23b01432e637d4f51d84cd7688e74f07e39b7a3e
rzsis <rz@rzsistemas.com.br>
2025-09-05 16:00:28 -0300

    Refatorado código de geração de embeddings

--- app/bll/embeddings_generate.py
+++ app/bll/embeddings_generate.py
@@ -21,12 +21,13 @@ class GenerateEmbeddings:
             db (Db): Database connection instance.
             localconfig: The localconfig module to read configuration.
         """
-        self.session = session
-        self.localconfig = localcfg
-        self.config = localcfg.read_config()  # Read config from the provided localconfig module
-        self.model_path = Path(self.config["model_path"])
-        self.max_length = self.config["max_length"]
-        self.batch_size = int(self.config["batch_size"])
+        self.session        = session
+        self.localconfig    = localcfg
+        self.config         = localcfg.read_config()  # Read config from the provided localconfig module
+        self.trainingPath   = localcfg.getTrainingPath()                
+        self.model_path     = Path(self.config["model_path"])
+        self.max_length     = self.config["max_length"]
+        self.batch_size     = int(self.config["batch_size"])
         self.textual_fields = {'Text'}  # Fields for embeddings
         self.metadata_fields = {'Classe', 'Id', 'CodClasse', 'QtdItens'}  # Include QtdItens
         self.tokenizer = None
@@ -115,28 +116,7 @@ class GenerateEmbeddings:
 
         return embeddings
 
-    def start(self):
-        """
-        Start the embedding generation process for the specified split.
-
-        Args:
-            split (str): Data split to process (default: 'train').
-        """
-        os.environ['CUDA_LAUNCH_BLOCKING'] = '1'  # Makes errors immediate
-        os.environ['TORCH_USE_CUDA_DSA'] = '1'  # Enables device-side assertions
-
-        trainingPath = self.localconfig.getTrainingPath()
-
-        os.makedirs(trainingPath, exist_ok=True)
-
-        # caso o arquivo train_final exista, então deve criar o train_final
-        split = "train"
-        embeddings_file_name = os.path.join(trainingPath, f"{split}_text.npy")
-        if (os.path.exists(embeddings_file_name)):
-            split = "train_final"
-            embeddings_file_name = os.path.join(trainingPath, f"{split}_text.npy")
-
-
+    def _fecth_data(self) -> list:
         # Define SQL query (deterministic with MIN(t.id))
         query = """
             SELECT MIN(t.id) AS Id,
@@ -147,7 +127,7 @@ class GenerateEmbeddings:
             FROM textos_treinamento t
             INNER JOIN classes c ON c.CodClasse = t.CodClasse
             WHERE LENGTH(TRIM(t.TxtTreinamento)) > 0
-            AND t.CodClasse IS NOT NULL
+            AND t.CodClasse IS NOT NULL            
             GROUP BY t.TxtTreinamento, t.CodClasse, c.Classe
         """
 
@@ -156,19 +136,20 @@ class GenerateEmbeddings:
         
             result = self.session.execute(text(query)).mappings().all()
             dados = [dict(row) for row in result]
-            self.session.close()
+            if not dados:            
+                raise RuntimeError("Não ha dados para gerar o modelo de embeddings.")                    
+                        
+            return dados        
         except Exception as e:            
             raise RuntimeError(f"Erro executando consulta no banco de dados: {e}")
+        
+    #Transforma os dados em embeddings
+    def _process_embeddings(self, dados, split) -> tuple:
 
-        if not dados:            
-            return RuntimeWarning("Não ha dados para gerar o modelo de embeddings.")
+        os.environ['CUDA_LAUNCH_BLOCKING'] = '1'  # Makes errors immediate
+        os.environ['TORCH_USE_CUDA_DSA'] = '1'  # Enables device-side assertions
 
-        print_with_time(f"Initial GPU memory: {torch.cuda.memory_allocated() / 1024**2:.2f} MB")
-
-        # Validate textual fields
-        if not self.textual_fields:            
-            raise RuntimeError("No textual fields defined")        
-
+        print_with_time(f"Initial GPU memory: {torch.cuda.memory_allocated() / 1024**2:.2f} MB")        
         # Prepare structures
         field_embeddings = {field: [] for field in self.textual_fields}
         field_metadata = {field: [] for field in self.metadata_fields}
@@ -176,7 +157,6 @@ class GenerateEmbeddings:
         invalid_reasons = Counter()
 
         print_with_time(f"Processando ({len(dados)} registros) e lotes de {self.batch_size} para o banco {split}...")
-
 
         # Process in batches
         for i in tqdm(range(0, len(dados), self.batch_size), desc="Generating embeddings in batches"):
@@ -229,7 +209,7 @@ class GenerateEmbeddings:
                         )
 
                 # Clear cache every 20 batches
-                if i % 20 == 0:
+                if i % 3 == 0:
                     torch.cuda.empty_cache()
 
             except torch.cuda.CudaError as e:
@@ -245,6 +225,10 @@ class GenerateEmbeddings:
         # Report
         print_with_time(f"Split : {len(dados) - skipped} examples processed, {skipped} examples skipped.")
 
+        # Validate textual fields
+        if not self.textual_fields:            
+            raise RuntimeError("No textual fields defined")       
+        
         # Validate
         if not field_embeddings:
             raise RuntimeError("Error: No textual fields processed for embeddings")
@@ -269,9 +253,12 @@ class GenerateEmbeddings:
         for field in field_metadata:
             field_metadata[field] = np.array(field_metadata[field], dtype=object)
 
-            
-        # Save embeddings
-        embeddings_file = os.path.join(trainingPath, f"{split}_text.npy")
+        return field_embeddings, field_metadata 
+
+    # Save embeddings and metadata to disk
+    def _save_embeddings(self,split,field_embeddings,field_metadata):
+
+        embeddings_file = os.path.join(self.trainingPath, f"{split}_text.npy")
         try:
             np.save(embeddings_file, field_embeddings['Text'])
             print_with_time(f"Embeddings for text saved to: {embeddings_file}")
@@ -280,7 +267,7 @@ class GenerateEmbeddings:
             raise RuntimeError(f"Error saving embeddings: {e}")
 
         # Save metadata
-        metadata_file = os.path.join(trainingPath, f"{split}_metadata.npz")
+        metadata_file = os.path.join(self.trainingPath, f"{split}_metadata.npz")
         try:
             np.savez(metadata_file, **field_metadata)
             print_with_time(f"Metadata (including {self.metadata_fields}) saved to: {metadata_file}")
@@ -288,4 +275,32 @@ class GenerateEmbeddings:
             print_with_time(f"Error saving metadata: {e}")
             raise RuntimeError(f"Error saving metadata: {e}")
 
-        print_with_time(f"✅ Split '{split}' processed successfully")
+        print_with_time(f"✅ Split '{split}' processado com sucesso")
+
+
+    #define o nome do split e como ele vai ser salvo
+    def _setSplit(self) -> str:        
+        os.makedirs(self.trainingPath, exist_ok=True)
+        # caso o arquivo train_final exista, então deve criar o train_final
+        split = "train"
+        embeddings_file_name = os.path.join(self.trainingPath, f"{split}_text.npy")
+        if (os.path.exists(embeddings_file_name)):
+            split = "train_final"
+            embeddings_file_name = os.path.join(self.trainingPath, f"{split}_text.npy")
+
+        return split
+
+    def start(self):
+        """
+        Start the embedding generation process for the specified split.
+
+        Args:
+            split (str): Data split to process (default: 'train').
+        """
+        split = self._setSplit()#define se vai ser train ou train_final
+
+        dados = self._fecth_data() #busca os dados no banco de dados
+     
+        field_embeddings,field_metadata = self._process_embeddings(dados,split)# processa os dados e transforma em embeddings
+            
+        self._save_embeddings(split,field_embeddings,field_metadata)# salva em disco os embeddings e metadados
